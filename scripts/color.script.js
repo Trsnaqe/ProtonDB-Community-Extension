702202e739ec7829eb3c27edc83a460cc0a2e663
Fudayl Cavus <fudaylcavus@gmail.com>
2023-09-01 21:32:30 +0300

    feat+refactor: new layout for html and script modularization

--- scripts/color.script.js
+++ scripts/color.script.js
@@ -1,66 +1,75 @@
+const DEFAULT_COLOR_PALETTE = {
+    platinumColor: "#B4C7DC",
+    goldColor: "#CFB53B",
+    silverColor: "#A6A6A6",
+    bronzeColor: "#CD7F32",
+    borkedColor: "#FF0000",
+    unknownColor: "#3D3D3D",
+};
+
+var colorPickers = document.querySelectorAll(".custom-color-picker");
+
+function getStoredColorPalette(callback) {
+    chrome.storage.local.get(DEFAULT_COLOR_PALETTE, callback);
+}
+
+function saveColors(event) {
+    const pickerFor = event.target.parentElement.getAttribute("picker-for");
+
+    let currentColorPalette;
+    getStoredColorPalette((colorPalette) => {
+        currentColorPalette = colorPalette;
+    });
+
+    const newColorPalette = {
+        ...currentColorPalette,
+        [`${pickerFor}Color`]: event.target.value,
+    };
+
+    chrome.storage.local.set(newColorPalette);
+
+    updateColorPickers(newColorPalette);
+}
+
+function loadColors() {
+    getStoredColorPalette(updateColorPickers);
+}
+
+function updateColorPickers(newColorPalette) {
+    colorPickers.forEach((colorPicker) => {
+        const pickerFor = colorPicker.getAttribute("picker-for");
+        const colorPickerInput = colorPicker.querySelector("input");
+
+        colorPickerInput.value = newColorPalette[`${pickerFor}Color`];
+        colorPicker.style.backgroundColor = newColorPalette[`${pickerFor}Color`];
+    });
+}
+
+
+function resetColors() {
+    if (!confirm("Are you sure you want to reset the colors to default values?")) {
+        return;
+    }
+    chrome.storage.local.set(DEFAULT_COLOR_PALETTE, loadColors);
+}
+
+
+
+/* Event Listeners */
+
 window.addEventListener("DOMContentLoaded", function () {
-  loadColors();
+    loadColors();
 });
 
-// Save the colors to local storage
-function saveColors() {
-  var platinumColor = document.getElementById("platinum").value || "#B4C7DC";
-  var goldColor = document.getElementById("gold").value || "#CFB53B";
-  var silverColor = document.getElementById("silver").value || "#A6A6A6";
-  var bronzeColor = document.getElementById("bronze").value || "#CD7F32";
-  var borkedColor = document.getElementById("borked").value || "#FF0000";
-  var unknownColor = document.getElementById("unknown").value || "#3D3D3D";
+colorPickers.forEach((colorPicker) => {
+    const colorPickerInput = colorPicker.querySelector("input");
 
-  chrome.storage.local.set({
-    platinumColor: platinumColor,
-    goldColor: goldColor,
-    silverColor: silverColor,
-    bronzeColor: bronzeColor,
-    borkedColor: borkedColor,
-    unknownColor: unknownColor,
-  });
-}
+    colorPicker.addEventListener("click", (event) => {
+        console.log(colorPickerInput);
+        colorPickerInput.click();
+    });
 
-// Load the colors from local storage
-function loadColors() {
-  chrome.storage.local.get(
-    {
-      platinumColor: "#B4C7DC",
-      goldColor: "#CFB53B",
-      silverColor: "#A6A6A6",
-      bronzeColor: "#CD7F32",
-      borkedColor: "#FF0000",
-      unknownColor: "#3D3D3D",
-    },
-    function (result) {
-      document.getElementById("platinum").value = result.platinumColor;
-      document.getElementById("gold").value = result.goldColor;
-      document.getElementById("silver").value = result.silverColor;
-      document.getElementById("bronze").value = result.bronzeColor;
-      document.getElementById("borked").value = result.borkedColor;
-      document.getElementById("unknown").value = result.unknownColor;
-    }
-  );
-}
+    colorPickerInput.addEventListener("input", saveColors);
+});
 
-// Reset the colors to their initial values
-function resetColors() {
-  chrome.storage.local.set(
-    {
-      platinumColor: "#B4C7DC",
-      goldColor: "#CFB53B",
-      silverColor: "#A6A6A6",
-      bronzeColor: "#CD7F32",
-      borkedColor: "#FF0000",
-      unknownColor: "#3D3D3D",
-    },
-    loadColors
-  );
-}
-
-// Save the colors whenever they change
-var colorInputs = document.querySelectorAll('input[type="color"]');
-colorInputs.forEach((input) => input.addEventListener("input", saveColors));
-document
-  .getElementById("resetColorsButton")
-  .addEventListener("click", resetColors);
+document.getElementById("resetColorsButton").addEventListener("click", resetColors);
